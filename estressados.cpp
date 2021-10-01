00f153302a48ffe2dd08eca02f61287e3385638a
well <well.santos@alu.ufc.br>
2021-10-01 13:09:34 -0300

    commit_estressados

--- estressados.cpp
+++ estressados.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 
-bool existe(std::vector<int>vet, int valor) {
-    for (int i = 0; i < vet.size(); i++)
+bool existe(std::vector<int>fila, int valor) {
+    
+    for (int i = 0; i < fila.size(); i++)
     {
-       if(vet[i] == valor){
+       if(fila[i] == valor){
            return true;
            return false;
        }
@@ -13,8 +14,48 @@ bool existe(std::vector<int>vet, int valor) {
 
 }
 
+int contar(std::vector<int>fila, int valor) {
+
+    int conta = 0;
+
+    for(int i = 0; i < fila.size(); i++)
+    {
+        if(fila[i] == valor){
+            conta ++;
+            
+        }
+    }
+    return conta;
+}
+
+int procurar_valor(std::vector<int> fila, int x) {
+
+    for(int i = 0; i < fila.size(); i++)
+    {
+        if(fila[i] == x){
+            return i;
+            
+      }else
+      {
+          for(int i = 0; i < fila.size(); i++)
+          {
+             return -1;
+          }
+          
+      }
+      
+      
+    }
+}
+
 int main(){
-    std::cout << existe({1, 2 ,3, 4, 5},7);
+    int num_humor = 0;
+
+    std::cin >> num_humor;
+    std::cout << existe({1, 2 ,3, 4, 5},num_humor);
+    std::cout << contar({2, 3 ,2, 3}, num_humor);
+    std::cout <<procurar_valor({1, 3, 6, 9, 8}, num_humor);
+
 
     return 0;
 }
