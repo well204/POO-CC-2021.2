0ba52d5b9abe9a2ced1e1e9006a1e527a5cdfbf7
well <well.santos@alu.ufc.br>
2021-10-01 15:39:26 -0300

    third_commit

--- estressados.cpp
+++ estressados.cpp
@@ -3,7 +3,8 @@
 
 bool existe(std::vector<int>fila, int valor) {
 
-    for (int i = 0; i < fila.size(); i++) {
+    for (int i = 0; i < fila.size(); i++)
+    {
        if(fila[i] == valor){
            return true;
            return false;
@@ -15,7 +16,8 @@ int contar(std::vector<int>fila, int valor) {
 
     int conta = 0;
 
-    for(int i = 0; i < fila.size(); i++) {
+    for(int i = 0; i < fila.size(); i++)
+    {
         if(fila[i] == valor){
             conta ++;
             
@@ -26,7 +28,8 @@ int contar(std::vector<int>fila, int valor) {
 
 int procurar_valor(std::vector<int> fila, int x) {
 
-    for(int i = 0; i < fila.size(); i++) {
+    for(int i = 0; i < fila.size(); i++)
+    {
         if(fila[i] == x){
             return i;    
        }else if(fila[i] != x){
@@ -37,7 +40,8 @@ int procurar_valor(std::vector<int> fila, int x) {
 
 int procurar_valor_apartir(std::vector<int> fila, int x, int inicio) {
     
-    for (int i = inicio; i < fila.size(); i++) {
+    for (int i = inicio; i < fila.size(); i++)
+    {
         if (fila[i] == x)
         {
             return i;
@@ -47,11 +51,12 @@ int procurar_valor_apartir(std::vector<int> fila, int x, int inicio) {
     
 }
 
-int procurar_menor(std::vector<int> fila) {
+int procurar_menor(std::vector<int> fila){
 
     int menor = fila[0];
 
-    for (int i = 0; i < fila.size(); ++i) {
+    for (int i = 0; i < fila.size(); ++i)
+    {
         if(fila[i] < menor)
         {
             menor = fila[i];
@@ -62,18 +67,20 @@ int procurar_menor(std::vector<int> fila) {
     return menor;
 }
 
-int procurar_menor_pos(std::vector<int> fila) {
-
-    int menor_posicao = 0;
-
-    for (int i = 0; i < fila.size(); ++i) {
-        if(fila[i] < fila[menor_posicao])
-        {
-            menor_posicao = i;
-        }
-    }
-
-    return menor_posicao;
+int procurar_menor_pos(std::vector<int> fila){ 
+ 
+    int menor = 0; 
+ 
+    for (int i = 0; i < fila.size(); i++) 
+    { 
+        if(fila[i] < fila[menor]) 
+        { 
+            menor = i; 
+         
+        } 
+    } 
+ 
+    return menor; 
 }
 
 int main(){
@@ -88,7 +95,7 @@ int main(){
     std::cout << procurar_valor({1, 3, 6, 9, 8, -1}, num_humor) << '\n';
     std::cout << procurar_valor_apartir({5, 3, -1, -50, -1, -99},num_humor, inicio) << '\n';
     std::cout << procurar_menor({5, 3, -1, -50, -1, -99}) << '\n';
-    std::cout << procurar_menor_pos({5, 3, -1, -1, -99});
+    std::cout << procurar_menor_pos({5, 3, -1, -50, -1, -99});
 
     return 0;
 }
